eb90d4696a633df3247cbcb637315f3430836928
Parsa_Shabanpour <parsash1380@gmail.com>
2023-12-08 20:43:46 +0330

    implement the functionality and the api of user authentication.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const { DB } = process.env;
 module.exports = function () {
-  mongoose.connect(DB).then(() => console.info(`connected to db successfully`));
+  mongoose
+    .connect(DB)
+    .then(() => console.info(`connected to db successfully`))
+    .catch(() => console.error("could not connect to db"));
 };
--- backend/index.js
+++ backend/index.js
@@ -1,122 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/error");
 require("dotenv").config();
-
-const chats = [
-  {
-    isGroupChat: false,
-    users: [
-      {
-        name: "John Doe",
-        email: "john@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-    ],
-    _id: "617a077e18c25468bc7c4dd4",
-    chatName: "John Doe",
-  },
-  {
-    isGroupChat: false,
-    users: [
-      {
-        name: "Guest User",
-        email: "guest@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-    ],
-    _id: "617a077e18c25468b27c4dd4",
-    chatName: "Guest User",
-  },
-  {
-    isGroupChat: false,
-    users: [
-      {
-        name: "Anthony",
-        email: "anthony@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-    ],
-    _id: "617a077e18c2d468bc7c4dd4",
-    chatName: "Anthony",
-  },
-  {
-    isGroupChat: true,
-    users: [
-      {
-        name: "John Doe",
-        email: "jon@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-      {
-        name: "Guest User",
-        email: "guest@example.com",
-      },
-    ],
-    _id: "617a518c4081150716472c78",
-    chatName: "Friends",
-    groupAdmin: {
-      name: "Guest User",
-      email: "guest@example.com",
-    },
-  },
-  {
-    isGroupChat: false,
-    users: [
-      {
-        name: "Jane Doe",
-        email: "jane@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-    ],
-    _id: "617a077e18c25468bc7cfdd4",
-    chatName: "Jane Doe",
-  },
-  {
-    isGroupChat: true,
-    users: [
-      {
-        name: "John Doe",
-        email: "jon@example.com",
-      },
-      {
-        name: "Piyush",
-        email: "piyush@example.com",
-      },
-      {
-        name: "Guest User",
-        email: "guest@example.com",
-      },
-    ],
-    _id: "617a518c4081150016472c78",
-    chatName: "Chill Zone",
-    groupAdmin: {
-      name: "Guest User",
-      email: "guest@example.com",
-    },
-  },
-];
+require("./config/db")();
 
 const app = express();
+app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
-app.get("/api/chats", async (req, res) => {
-  res.send({ data: chats });
-});
+
+app.use("/api/user", userRoutes);
+// app.get("/api/chats", async (req, res) => {
+//   res.send({ data: chats });
+// });
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
 const port = process.env.PORT;
 app.listen(port, console.log(`server started on port ${port}`));
-
-
